f60cd1497b00f819142f4c1d5973c78064d7b445
srazzz <sravs.vadlamanu@gmail.com>
2023-02-08 15:21:27 +0530

    test commit

--- App.tsx
+++ App.tsx
@@ -7,22 +7,14 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
+import MainScreen from './screens/mainScreen';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
@@ -45,7 +37,7 @@ function App(): JSX.Element {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      
+      <MainScreen/>
     </SafeAreaView>
   );
 }
