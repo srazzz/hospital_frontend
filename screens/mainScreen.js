aab97f86930d262c0b08cde3c822892f1162f30c
srazzz <sravs.vadlamanu@gmail.com>
2023-02-08 16:24:33 +0530

    created mainscreen

--- screens/mainScreen.js
+++ screens/mainScreen.js
@@ -2,11 +2,22 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const MainScreen = () => {
     return (
-        <View>
-            <Text>Doctor</Text>
-            <Text>patient</Text>
+        <View style={styles.background}>
+            <Text style={styles.text}>Doctor</Text>
+            <Text style={styles.text}>patient</Text>
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    background : {
+      backgroundColor : "#D3E6E5",
+      height : "100%",
+      width : "100%",
+    },
+    text : {
+        color : "black",
+        
+    }
+    })
 export default MainScreen;
